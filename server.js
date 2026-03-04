5094d4fcdbe919135943f79f9b605ae68c2c9f0e
Lanhak <akklanh84@gmail.com>
2026-03-05 00:07:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -547,13 +547,14 @@ if (
             aesKey
         );
 
-        return sendJSON(res, {
-            ok: true,
-            ek: encryptedKey.toString("base64"),
-            iv: iv.toString("base64"),
-            ct: encryptedData.toString("base64"),
-            tag: tag.toString("base64")
-        });
+        res.writeHead(200, { "Content-Type": "text/plain" });
+
+return res.end(
+    encryptedKey.toString("base64") + "." +
+    iv.toString("base64") + "." +
+    encryptedData.toString("base64") + "." +
+    tag.toString("base64")
+);
 
     } catch (err) {
         return sendJSON(res, { ok:false });
