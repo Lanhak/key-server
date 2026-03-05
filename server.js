502820258027047d71e65d452d3e8178f6387ebd
Lanhak <akklanh84@gmail.com>
2026-03-05 21:32:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -621,10 +621,14 @@ if (
     const nowTime = now();
 
     // Nếu chưa có expire hoặc expire <= now thì set lại 24h
-    if (!record.expires_at || record.expires_at <= nowTime) {
-        record.expires_at = nowTime + 86400;
-        saveDB();
-    }
+    if (!record.expires_at) {
+    record.expires_at = nowTime + 86400; // chỉ set lần đầu
+    saveDB();
+}
+
+if (record.expires_at <= nowTime) {
+    return sendJSON(res, { ok:false, reason: "expired" });
+}
 
     const remaining = record.expires_at - nowTime;
 
